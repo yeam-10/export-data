992ea89fa0c159df2b9fcc0c24f4ea5e8a05c4af
dev-91 <yeam-10@anyeimar>
2021-09-23 22:10:14 -0400

    [ADD] Se agrega boton para exportar archivo

---
+++ __manifest__.py
@@ -0,0 +1,18 @@
+{
+    'name': "Button",
+    'version': '1.0',
+    'depends': ['base','mail','product','sale','stock'],
+    'author': "Alfa Soluciones",
+    'category': 'Category',
+    'description': """
+     Este modulo 
+    """,
+    # data files always loaded at installation
+    'data': [
+
+        'views/button_export_data_view.xml',
+       
+       
+    ]
+   
+}
