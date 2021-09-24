d93d10d337a7c5a033ee2288626fb34e251f2889
dev-91 <yeam-10@anyeimar>
2021-09-23 22:33:04 -0400

    [MOD] manisfest

--- __manifest__.py
+++ __manifest__.py
@@ -1,18 +1,30 @@
+# -*- coding: utf-8 -*-
 {
-    'name': "Button",
-    'version': '1.0',
-    'depends': ['base','mail','product','sale','stock'],
-    'author': "Alfa Soluciones",
-    'category': 'Category',
-    'description': """
-     Este modulo 
+  'name':'Button',
+  'summary': """
+        Alfa Soluciones
     """,
-    # data files always loaded at installation
-    'data': [
+  'description': '''
+          Este módulo exportar data.
+	  ''',
+  'version':'1.0',
+  'author':' Alfa Soluciones',
+  'website': "",
+  'application': False,
+  'data': [
+    #'security/security.xml',
+    # 'security/ir.model.access.csv',
+    # 'report/clinica_visualizer_report.xml',
+    #'report/clinica_visualizer_report_template.xml',
+    #'views/clinica_control_history_view.xml',
+    'views/button_export_data_view.xml',
+    # 'views/inherit_doctor_calendar_view.xml',
+    # 'views/invisible_menu.xml',
+    #'views/clinica_record_list_visualizer_view.xml',
+    # 'views/inherit_prescription.xml',
+  ],
 
-        'views/button_export_data_view.xml',
-       
-       
-    ]
-   
+
+  'category': '',
+  'depends': ['base'],
 }
